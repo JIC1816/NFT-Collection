e70ff0cd1f88bd20b086988858b3aae3daa56831
jic1816 <puca.alejandro@gmail.com>
2022-06-11 04:26:15 -0300

    Primer Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xFE0aEE87bf69556336aC21B9e5304eF29F80066d";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
